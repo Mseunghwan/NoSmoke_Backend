57d315f958c551873f2845c0a243f336353570e3
Mseunghwan <minccc42710@gmail.com>
2025-07-01 16:54:28 +0900

    feat : initial commit

---
+++ src/main/java/org/example/nosmoke/NoSmokeApplication.java
@@ -0,0 +1,13 @@
+package org.example.nosmoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoSmokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoSmokeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/nosmoke/NoSmokeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nosmoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoSmokeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
