53d54a525aa3339a9e9ade774f6bc1d735061533
Mseunghwan <minccc42710@gmail.com>
2025-09-14 23:48:08 +0900

    Feat : 흡연정보 등록

--- src/main/java/org/example/nosmoke/service/user/CutsomUserDetailsService.java
+++ src/main/java/org/example/nosmoke/service/user/CutsomUserDetailsService.java
@@ -36,7 +36,7 @@ public class CutsomUserDetailsService implements UserDetailsService {
     // DB에 User 값 존재한다면 UserDetails 객체로 만들어 리턴
     private UserDetails createUserDetails(User user) {
         return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getEmail())
+                .username(String.valueOf(user.getId())) // 이메일 대신 회원 ID를 UserID로 --> 이메일 사용하면 input string 에러 발생
                 .password(user.getPassword())
                 .authorities(Collections.emptyList()) // 역할 설정 부분, 우선 비워 둠
                 .build();
